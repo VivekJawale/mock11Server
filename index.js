86e117dc268a8ca94ec025446b28492b4187bcbb
VivekJawale <vivekjawale41@gmail.com>
2023-02-25 12:41:28 +0530

    added

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const express = require('express');
-const cors= require('cors');
-const PORT=process.env.PORT || 8080; 
-const connect= require('./src/config/db');
-
+const cors = require('cors');
+const PORT = process.env.PORT || 8080;
+const connect = require('./src/config/db');
+const userrouter = require("./src/features/Auth/user.route")
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("", userrouter)
 
-
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("hello world!");
 })
 
-app.listen(PORT,async(req,res)=>{
+app.listen(PORT, async (req, res) => {
     try {
         await connect();
         console.log(`http://localhost:${PORT}`);
